55dea7a9449abdb758444cdd74f7df7995ba991a
Antonio Teixeira <antonio.teixeira@blinctek.com>
2022-06-20 11:17:03 -0400

    feat: update markers every ten minutes

--- lib/map/view/cubit/fire/fire_cubit.dart
+++ lib/map/view/cubit/fire/fire_cubit.dart
@@ -4,39 +4,44 @@ import 'package:free_fire_location/map/data/repositories/fire_repository.dart';
 import 'package:free_fire_location/map/models/fire_page.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:meta/meta.dart';
+import 'package:cron/cron.dart';
 
 part 'fire_state.dart';
 
 class FireCubit extends Cubit<FireState> {
   final _mapRepository = FireRepository();
+  final _cron = Cron();
+  late final List<Marker> markers;
   FireCubit() : super(FireInitial());
 
-  Future<List<Marker>> _generateMarkers(fireInfo) async {
+  void getFireInfo() async {
     final customMarker = await BitmapDescriptor.fromAssetImage(
       const ImageConfiguration(size: Size(20, 20)),
       "assets/images/splash.png",
     );
 
-    List<Marker> markers = fireInfo.coordinatesList.map((item) {
-      return Marker(
-          markerId: MarkerId('${item.latitude}${item.longitude}${item.date}'),
-          position: LatLng(item.latitude, item.longitude),
-          infoWindow: InfoWindow(title: '${item.satelliteName} - ${item.date}'),
-          icon: customMarker);
-    }).toList();
-
-    return markers;
-  }
-
-  void getFireInfo() async {
-    emit.call(FireLoading());
-
     try {
       final FirePage fireInfo = await _mapRepository.getFireLocations();
 
-      emit.call(FireSuccess(markers: await _generateMarkers(fireInfo)));
+      List<Marker> markers = fireInfo.coordinatesList.map((item) {
+        return Marker(
+            markerId: MarkerId('${item.latitude}${item.longitude}${item.date}'),
+            position: LatLng(item.latitude, item.longitude),
+            infoWindow:
+                InfoWindow(title: '${item.satelliteName} - ${item.date}'),
+            icon: customMarker);
+      }).toList();
+
+      emit.call(FireSuccess(markers: markers));
     } catch (e) {
       emit.call(FireError());
     }
+  }
+
+  void startRequesting() {
+    getFireInfo();
+    _cron.schedule(Schedule.parse('*/10 * * * *'), () async {
+      getFireInfo();
+    });
   }
 }
