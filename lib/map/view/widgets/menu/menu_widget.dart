b132f18362548474bf516ca6b46f835d79c45f05
Rodolfo Abrahão <roabrahao5@gmail.com>
2022-06-24 00:28:32 -0300

    styling menu texts

--- lib/map/view/widgets/menu/menu_widget.dart
+++ lib/map/view/widgets/menu/menu_widget.dart
@@ -18,20 +18,42 @@ class MenuWidget extends StatelessWidget {
                   width: 100,
                   height: 100,
                   child: Image.asset('assets/images/splash.png')),
-              const Center(child: Text('FreeFire Locator')),
+              const SizedBox(height: 10),
+              const Center(
+                  child: Text('FreeFire Locator',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: AppColors.white,
+                          fontWeight: FontWeight.bold))),
               const SizedBox(height: 57),
               GestureDetector(
-                child: const Text('Adicionar local padrão'),
+                child: const Text(
+                  'Adicionar local padrão',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: AppColors.white,
+                  ),
+                ),
                 onTap: () {},
                 onLongPress: () {},
               ),
+              const SizedBox(height: 20),
               GestureDetector(
-                child: const Text('Configurações'),
+                child: const Text('Configurações',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: AppColors.white,
+                    )),
                 onTap: () {},
                 onLongPress: () {},
               ),
+              const SizedBox(height: 20),
               GestureDetector(
-                child: const Text('Sobre'),
+                child: const Text('Sobre',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: AppColors.white,
+                    )),
                 onTap: () {},
                 onLongPress: () {},
               ),
