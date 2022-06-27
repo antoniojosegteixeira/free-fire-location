fbe5b31c7f8187dc54e447a0162520e5acad4033
Rodolfo <rodolfo.abrahao@blinctek.com>
2022-06-27 15:38:20 -0300

    resolving conflicts

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:free_fire_location/map/view/cubit/fire/fire_cubit.dart';
 import 'package:free_fire_location/map/view/pages/map.dart';
 import 'package:free_fire_location/map/view/pages/splash.dart';
@@ -10,14 +11,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final fireCubit = FireCubit();
 
-<<<<<<< HEAD
-    return MaterialApp(
-      title: 'FreeFire Locator',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: const MapPage(),
-=======
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (_) => fireCubit..startRequesting()),
@@ -33,7 +26,6 @@ class MyApp extends StatelessWidget {
           '/map': (context) => const MapPage(),
         },
       ),
->>>>>>> a3cb0508cf2a24d1c238fd72c64fd2af4ea7e7ff
     );
   }
 }
