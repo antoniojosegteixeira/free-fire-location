2c2eb77e060c52603af2108b11b0bfadb26437f4
Antonio Teixeira <antonio.teixeira@blinctek.com>
2022-07-13 08:46:17 -0400

    fix: correctly schedule firebase function

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -3,53 +3,19 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-/*
+
 exports.scheduledFunctionCrontab = functions.pubsub
-  .schedule("every 2 minutes")
-  .timeZone("America/New_York")
+  .schedule("every 2 hours")
+  .timeZone("Europe/London")
   .onRun((context) => {
-    const date = Date.now();
-    const nowUtc = Date.UTC(
-      date.getUTCFullYear(),
-      date.getUTCMonth(),
-      date.getUTCDate(),
-      date.getUTCHours(),
-      date.getUTCMinutes(),
-      date.getUTCSeconds()
-    );
-    const messagesRef = admin.database().ref("/markersMatrix/userMarkers");
-    messagesRef.once("value", (snapshot) => {
-      snapshot.forEach((child) => {
-        console.log("child ${child}");
-        const postDate = Date.parse(child.val()["date"]);
-        const postLimit = new Date(
-          dateNow.getUTCFullYear(),
-          dateNow.getUTCMonth(),
-          dateNow.getUTCDate(),
-          dateNow.getUTCHours(),
-          dateNow.getUTCMinutes() + 2,
-          dateNow.getUTCSeconds()
-        );
-        if (nowUtc >= postLimit) {
-          child.ref.set(null);
-        }
-      });
-    });
-    response.send("Deleted old entries");
-  });
-
-  */
-
-
-exports.deleteOldEntries = functions.https.onRequest((request, response) => {
-
-	const dateNow = new Date();
+	
+	const dateNow = new Date(context.timestamp);
 	const dateTwoHoursAgo = new Date(
 		dateNow.getUTCFullYear(),
 		dateNow.getUTCMonth(),
 		dateNow.getUTCDate(),
-		dateNow.getUTCHours(),
-		dateNow.getUTCMinutes() + 2,
+		dateNow.getUTCHours() - 2,
+		dateNow.getUTCMinutes(),
 		dateNow.getUTCSeconds()
 	);
     const messagesRef = admin.database().ref();
@@ -67,3 +33,34 @@ exports.deleteOldEntries = functions.https.onRequest((request, response) => {
 
     response.send("OK");
   });
+
+
+
+exports.deleteOldEntries = functions.https.onRequest((request, response) => {
+	console.log('CFOSIAJDSAASIOA');
+	const dateNow = new Date();
+	const dateTwoHoursAgo = new Date(
+		dateNow.getUTCFullYear(),
+		dateNow.getUTCMonth(),
+		dateNow.getUTCDate(),
+		dateNow.getUTCHours() - 2,
+		dateNow.getUTCMinutes(),
+		dateNow.getUTCSeconds()
+	);
+
+	
+    const messagesRef = admin.database().ref();
+	var itemRef = messagesRef.child("/markersMatrix/userMarkers");
+	
+	itemRef.once("value", function(snapshot) {
+		snapshot.forEach(function(child) {
+			const date = new Date(child.val().data);
+			if (date <= dateTwoHoursAgo) {
+				child.ref.remove();
+			}
+		});
+	});
+    response.send("OK");
+  });
+
+ 
